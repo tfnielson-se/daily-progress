10ce2693b15d4b9f4ac049da225f1d77fa23be16
Thomas <tfnielson.se@gmail.com>
2023-05-11 13:29:17 -0600

    solved hacker rank staircase

--- python/hacker-rank-staircase.py
+++ python/hacker-rank-staircase.py
@@ -12,21 +12,21 @@ import sys
 # The function accepts INTEGER n as parameter.
 #
 
+
 def staircase(n):
     # Write your code here
     hashtags = range(1, n+1)
     hashtags_arr = [num * '#' for num in hashtags]
-    
+
     spaces = range(n)
     range_spaces = [(space) for space in spaces]
     range_spaces.reverse()
-    
+
     spaces_arr = [space * ' ' for space in range_spaces]
-    
-    
+
     for spaces, hashtags in zip(spaces_arr, hashtags_arr):
         print(spaces + hashtags)
-    
+
 
 if __name__ == '__main__':
     n = int(input().strip())
